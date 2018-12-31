161591eb737bb22336e41e8996ebc5a9c0f118f8
oginoyuki <sakurakotori0520@gmail.com>
2019-01-01 06:25:04 +0800

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -5,7 +5,7 @@ Spyder Editor
 This is a temporary script file.
 """
 
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy
 f = open('dataset.txt')
 n = int(f.readline())
@@ -32,118 +32,162 @@ for x in f.readline().strip().split('\t'):
     j = j + 1
 
 #算出Xi的總和----------------------------------------------------------
-#K=200 #200種可能
-X = numpy.random.randint(0,21,size=(n,1))
+K=400 #200種可能
+X = numpy.random.randint(0,21,size=(K,n))
 #print(X)
-X_sum = numpy.zeros((1,1),int)
-j = 0
-for j in range(n):
-    X_sum = X_sum + X[j]
-#print(X_sum)
+X_sum = [0]*K
+for k in range(K):
+    j = 0
+    for j in range(n):
+        X_sum[k] = X_sum[k] + X[k][j]
+    #print(X_sum[k])
 
 #Xi總和的限制式-----------------------------------------------------------
-while(X_sum > h):
-    point=0 #見HW_help1
-    for i in range(n): #X.size=10
-        if( X[point] < X[i] ):
-            point = i #做位置紀錄，可以找到最大值的位置
-    #print("The largest value", X[point], "is in the position",point)
-    X[point] = 50 - (X_sum - X[point])
-    if(X[point] < 0):
-        X[point] = 0
-    X_sum = 0
-    for j in range(n):
-        X_sum = X_sum + X[j]
-    mux_sum = numpy.zeros((1,1),float) #算出uiXi的總和------------------------
-    for j in range(n):
-        mux_sum = mux_sum + (mu[j] * X[j])
-    while(mux_sum < rho): #uixi總和的限制式
-        X = numpy.random.randint(0,21,size=(n,1))
-        X_sum = numpy.zeros((1,1),int)
-        j = 0
+mux_sum = [0]*K
+for k in range(K):
+    while(X_sum[k] > h):
+        point=0 #見HW_help1
+        for i in range(n): #X.size=10
+            if( X[k][point] < X[k][i] ):
+                point = i #做位置紀錄，可以找到最大值的位置
+        #print("The largest value", X[point], "is in the position",point)
+        X[k][point] = 50 - (X_sum[k] - X[k][point])
+        if(X[k][point] < 0):
+            X[k][point] = 0
+        X_sum[k] = 0
         for j in range(n):
-            X_sum = X_sum + X[j]
+            X_sum[k] = X_sum[k] + X[k][j]
+        #算出uiXi的總和--------------------------------------------------
+        for j in range(n):
+            mux_sum[k] = mux_sum[k] + (mu[j] * X[k][j])
+        while(mux_sum[k] < rho): #uixi總和的限制式
+            X = numpy.random.randint(0,21,size=(K,n))
+            X_sum = [0]*K
+            j = 0
+            for j in range(n):
+                X_sum[k] = X_sum[k] + X[k][j]    
+    
+    while(X_sum[k] < h):
+        point=0 #見HW_help1
+        for i in range(n): #X.size=10
+            if( X[k][point] > X[k][i] ):
+                point = i #做位置紀錄，可以找到最小值的位置
+        #print("The smallest value", X[point], "is in the position",point)
+        X[k][point] = X[k][point] + (50 - X_sum[k])
+        if(X[k][point] > 20):
+            X[k][point] = 20
+        X_sum[k] = 0
+        for j in range(n):
+            X_sum[k] = X_sum[k] + X[k][j]
+        #算出uiXi的總和------------------------------------------------------
+        for j in range(n):
+            mux_sum[k] = mux_sum[k] + (mu[j] * X[k][j])
+        while(mux_sum[k] < rho): #uixi總和的限制式
+            X = numpy.random.randint(0,21,size=(K,n))
+            X_sum = [0]*K
+            j = 0
+            for j in range(n):
+                X_sum[k] = X_sum[k] + X[k][j]
 #print(X)
 #print(X_sum)
 #print(mux_sum)
-
-while(X_sum < h):
-    point=0 #見HW_help1
-    for i in range(n): #X.size=10
-        if( X[point] > X[i] ):
-            point = i #做位置紀錄，可以找到最小值的位置
-    #print("The smallest value", X[point], "is in the position",point)
-    X[point] = X[point] + (50 - X_sum)
-    if(X[point] > 20):
-        X[point] = 20
-    X_sum = 0
-    for j in range(n):
-        X_sum = X_sum + X[j]
-    mux_sum = numpy.zeros((1,1),float) #算出uiXi的總和------------------------
-    for j in range(n):
-        mux_sum = mux_sum + (mu[j] * X[j])
-    while(mux_sum < rho): #uixi總和的限制式
-        X = numpy.random.randint(0,21,size=(n,1))
-        X_sum = numpy.zeros((1,1),int)
+            
+#算目標式
+Objective = [0]*K
+for k in range(K):
+    for i in range(n):
         j = 0
         for j in range(n):
-            X_sum = X_sum + X[j]
-#print(X)
-#print(X_sum)
-#print(mux_sum)   
+            Objective[k] = Objective[k] + (sigma[i][j] * X[k][i] * X[k][j])
+#print(Objective)
 
-#HW1-------------------------------------------------------------------   
-"""
-Ite=50
-K=200 #200種可能
-X=numpy.random.randint(0,21,size=(K,n)) #範圍介於0~100
-Objective=[0]*K
-
-for k in range(K):
-    while(( mu * X[k][n] < rho ) or (X[k][n]!=h)):
-        for i in range(n):
-            X[k][i]=numpy.random.randint(0,21)
-        #X[k][1]=np.random.randint(0,21)
-        #X[k][2]=np.random.randint(0,21)
-        #X[k][3]=np.random.randint(0,21)
-        #X[k][4]=np.random.randint(0,21)
-#print(X)
-        
-for k in range(K):
-    Objective[k]=sigma*X[k][i]*X[k][j]
-    #print(Objective[k])
-
+#找目標式最小值-------------------------------------------------------------
 Point_k=0
 for k in range(1,k):
-    if(Objective[k]<Objective[Point_k]):
+    if(Objective[k] < Objective[Point_k]):
         Point_k=k
+#print(Objective[Point_k])
 
-print("Incumbent solution:", end="\t")
-print("x1=",X[Point_k][0],":\t x2=",X[Point_k][1],":\t x3=",X[Point_k][2],":\t x4=",X[Point_k][3],":\t x5=",X[Point_k][4])
+Ite = 50
+x = [0] *Ite
+y = [0] *Ite
 
-x = [0] * Ite
-y = [0] * Ite
-
-for iteration in range (Ite): #重複執行50次
-    for k in range (0,k):
-        if(k!=Point_k): #只要不是最好的那一個Point_k就重取
-            X[k][0]=np.random.randint(0,101)
-            X[k][1]=np.random.randint(0,101)
-            X[k][2]=np.random.randint(0,101)
-            X[k][3]=np.random.randint(0,101)
-            X[k][4]=np.random.randint(0,101)
-            while((X[k][0]-X[k][1]<23) or (X[k][1]-2*X[k][2]<25) or (X[k][2]+X[k][3]<26) or (X[k][3]-X[k][4]<12)):
-                X[k][0]=np.random.randint(0,101)
-                X[k][1]=np.random.randint(0,101)
-                X[k][2]=np.random.randint(0,101)
-                X[k][3]=np.random.randint(0,101)
-                X[k][4]=np.random.randint(0,101)
-            Objective[k]=3*X[k][0]+10*X[k][1]+5*X[k][2]+2*X[k][3]-3225*X[k][4]-3*X[k][0]*X[k][2]-4*X[k][1]*X[k][2]+6*X[k][2]*X[k][2]
+for iteration in range(Ite):
+    for k in range (K):
+        #print(k,Point_k)
+        if(k!=Point_k):
+            X_sum[k] = 0
+            mux_sum[k] = 0
+            Objective[k] = 0
+            X[k][0] = numpy.random.randint(0,21)
+            X[k][1] = numpy.random.randint(0,21)
+            X[k][2] = numpy.random.randint(0,21)
+            X[k][3] = numpy.random.randint(0,21)
+            X[k][4] = numpy.random.randint(0,21)
+            X[k][5] = numpy.random.randint(0,21)
+            X[k][6] = numpy.random.randint(0,21)
+            X[k][7] = numpy.random.randint(0,21)
+            X[k][8] = numpy.random.randint(0,21)
+            X[k][9] = numpy.random.randint(0,21)
+            
+            #print('Point_k=',Point_k,'k=',k)
+            #print(X[k][0])
+            for j in range(n):
+                X_sum[k] = X_sum[k] + X[k][j]
+            #print(X_sum[k])
+            while(X_sum[k] > h):
+                point=0 
+                for i in range(n): 
+                    if( X[k][point] < X[k][i] ):
+                        point = i #做位置紀錄，可以找到最大值的位置
+                X[k][point] = 50 - (X_sum[k] - X[k][point])
+                if(X[k][point] < 0):
+                    X[k][point] = 0
+                X_sum[k] = 0
+                for j in range(n):
+                    X_sum[k] = X_sum[k] + X[k][j]
+            #print(X_sum[k])
+               #算出uiXi的總和--------------------------------------------------
+                for j in range(n):
+                    mux_sum[k] = mux_sum[k] + (mu[j] * X[k][j])
+                while(mux_sum[k] < rho): #uixi總和的限制式
+                    X = numpy.random.randint(0,21,size=(K,n))
+                    X_sum = [0]*K
+                    j = 0
+                    for j in range(n):
+                        X_sum[k] = X_sum[k] + X[k][j]    
+            #print(X_sum[k])
+            while(X_sum[k] < h):
+                point=0 
+                for i in range(n): 
+                    if( X[k][point] > X[k][i] ):
+                        point = i #做位置紀錄，可以找到最小值的位置
+                X[k][point] = X[k][point] + (50 - X_sum[k])
+                if(X[k][point] > 20):
+                    X[k][point] = 20
+                X_sum[k] = 0
+                for j in range(n):
+                    X_sum[k] = X_sum[k] + X[k][j]
+                #算出uiXi的總和------------------------------------------------------
+                for j in range(n):
+                    mux_sum[k] = mux_sum[k] + (mu[j] * X[k][j])
+                while(mux_sum[k] < rho): #uixi總和的限制式
+                    X = numpy.random.randint(0,21,size=(K,n))
+                    X_sum = [0]*K
+                    j = 0
+                    for j in range(n):
+                        X_sum[k] = X_sum[k] + X[k][j]
+                
+            for i in range(n):
+                for j in range(n):
+                    Objective[k] = Objective[k] + (sigma[i][j] * X[k][i] * X[k][j])
+            #print(Objective[k])
         for k in range (0,K):
-            if(Objective[k]<Objective[Point_k]):
-                Point_k=k
+            if(Objective[k] < Objective[Point_k]):
+                Point_k = k
         print("Incumbent solution:",iteration+1, end="\t")
-        print("x1=",X[Point_k][0],":\t x2=",X[Point_k][1],":\t x3=",X[Point_k][2],":\t x4=",X[Point_k][3],":\t x5=",X[Point_k][4])
+        for j in range(n):
+            print("xj=",X[Point_k][j],"\t")
         x[iteration]=iteration+1
         y[iteration]=Objective[Point_k]
 #Display the visualization results
@@ -151,4 +195,4 @@ plt.plot(x, y) #line plot線圖
 plt.ylabel('Objective value')
 plt.xlabel('Iterations')
 plt.show()
-"""
+print(Objective[Point_k])
